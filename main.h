4f6f347347806ae4d7f2d5549a0b58fb00ada744
alexisGalarzaSustaita <a355316@alumnos.uaslp.mx>
2024-10-30 03:54:49 +0000

    Add changes in main.h

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 // Structure Node
 typedef struct Node {
     char *ruleIdentifier;
-    char *production; 
+    char *production;
     struct Node *next;
 } Node;
 
@@ -15,11 +15,15 @@ typedef struct Node {
 Node *createNode(const char *ruleIdentifier, const char *production);
 void appendNode(Node **head, const char *ruleIdentifier, const char *production);
 void freeLinkedList(Node *head);
-Node* createLinkedList(FILE *file);
+Node *createLinkedList(FILE *file);
 void printList(Node *head);
 
 void splitLine(const char *line, char *ruleIdentifier, char *production);
 
-Node* findNode(Node *head, const char *ruleIdentifier);
+Node *findNode(Node *head, const char *ruleIdentifier);
 void appendProduction(Node *node, const char *production);
 void appendOrUpdateNode(Node **head, const char *ruleIdentifier, const char *production);
+
+void eliminaEspacios(Node *head);
+void keys(Node *head);
+void processProductions(Node *head);
