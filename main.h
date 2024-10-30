145287658979a19bb38507cf8829f096d2a850d3
alexisGalarzaSustaita <a355316@alumnos.uaslp.mx>
2024-10-29 21:49:35 -0600

    Create main.h
    Add main.h

---
+++ main.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 100
+
+// Structure Node
+typedef struct Node {
+    char *ruleIdentifier;
+    char *production; 
+    struct Node *next;
+} Node;
+
+// Function declarations
+Node *createNode(const char *ruleIdentifier, const char *production);
+void appendNode(Node **head, const char *ruleIdentifier, const char *production);
+void freeLinkedList(Node *head);
+Node* createLinkedList(FILE *file);
+void printList(Node *head);
+
+void splitLine(const char *line, char *ruleIdentifier, char *production);
+
+Node* findNode(Node *head, const char *ruleIdentifier);
+void appendProduction(Node *node, const char *production);
+void appendOrUpdateNode(Node **head, const char *ruleIdentifier, const char *production);
